4d18f60af309deff9635d3d395bbc6c74347c17a
Scott Frolich <scottfrolich@sfrolic-E758.local>
2017-05-03 14:28:44 -0700

    update azure stanza start script

--- 1.6/azure/mapr-cloud-configure.sh
+++ 1.6/azure/mapr-cloud-configure.sh
@@ -4,6 +4,17 @@
 MEP=$1
 CLUSTER_NAME=$2
 CLUSTER_ADMIN_PASSWORD=$3
+THREE_DOT_SUBNET_PRIVATE=$4
+START_OCTET=$5
+NODE_COUNT=$6
+RESULT=""
+
+echo "MEP: ${MEP}"
+echo "CLUSTER_NAME: ${CLUSTER_NAME}"
+echo "CLUSTER_ADMIN_PASSWORD: <hidden>"
+echo "THREE_DOT_SUBNET_PRIVATE: ${THREE_DOT_SUBNET_PRIVATE}"
+echo "START_OCTET: ${START_OCTET}"
+echo "NODE_COUNT: ${NODE_COUNT}"
 
 STANZA_URL="https://raw.githubusercontent.com/mapr/mapr-cloud-templates/master/1.6/azure/mapr-core.yml"
 SERVICE_TEMPLATE="template-20-drill"
@@ -14,6 +25,31 @@ M_USER=mapr
 # TODO: Need to get the core version in here
 MAPR_CORE=5.2.1
 H=$(hostname -f)
+
+function create_node_list() {
+    local current_node=$1
+    local last_node
+    let last_node=current_node+$2-1
+    local mapr_nodes="["
+
+    while true; do
+        [ $current_node -gt $last_node ] && break
+
+        if [ $current_node -eq $last_node ]; then
+            mapr_nodes="$mapr_nodes\"$3$current_node\"]"
+        else
+            mapr_nodes="$mapr_nodes\"$3$current_node\", "
+        fi
+
+        let current_node=$current_node+1
+    done
+
+    RESULT=$mapr_nodes
+}
+
+create_node_list $START_OCTET $NODE_COUNT $THREE_DOT_SUBNET_PRIVATE
+NODE_LIST=$RESULT
+
 # TODO: SWF: I don't see REPLACE_THIS in properties.json anymore. Not needed?
 #sed -i -e "s/REPLACE_THIS/$H/" $M_HOME/data/properties.json
 service mapr-installer start
@@ -37,7 +73,7 @@ echo "config.ssh_password=UsingCloud4MapR " >> $input
 echo "config.mep_version=${MEP} " >> $input
 echo "config.cluster_name=${CLUSTER_NAME} " >> $input
 # TODO: SWF need to find the IPs based on subnet and installer's private IP
-echo "config.hosts=[\"28.1.8.4\", \"28.1.8.5\"] " >> $input
+echo "config.hosts=$NODE_LIST " >> $input
 
 CMD="cd $M_HOME; bin/mapr-installer-cli install -v -f -n -t $STANZA_URL -u $M_USER:${CLUSTER_ADMIN_PASSWORD}@localhost:9443 -o @$input"
 echo $CMD > /tmp/cmd
