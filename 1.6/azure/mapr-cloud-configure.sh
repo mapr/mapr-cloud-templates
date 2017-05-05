c6cafefa18a9d8100181e89b5ba08dfed7ec9769
Scott Frolich <scottfrolich@sfrolic-E758.local>
2017-05-05 09:43:29 -0700

    update user path

--- 1.6/azure/mapr-cloud-configure.sh
+++ 1.6/azure/mapr-cloud-configure.sh
@@ -23,7 +23,7 @@ STANZA_URL="https://raw.githubusercontent.com/mapr/mapr-cloud-templates/master/1
 SERVICE_TEMPLATE="template-20-drill"
 STATUS="SUCCESS"
 
-M_HOME=/opt/mapr/installer
+MAPR_HOME=/opt/mapr/installer
 MAPR_USER=mapr
 
 # TODO: Need to get the core version in here
@@ -53,20 +53,20 @@ create_node_list $START_OCTET $NODE_COUNT $THREE_DOT_SUBNET_PRIVATE
 NODE_LIST=$RESULT
 echo "NODE_LIST: $NODE_LIST"
 
-. $M_HOME/build/installer/bin/activate
+. $MAPR_HOME/build/installer/bin/activate
 
 # TODO: SWF: I don't see REPLACE_THIS in properties.json anymore. Not needed?
-#sed -i -e "s/REPLACE_THIS/$H/" $M_HOME/data/properties.json
+#sed -i -e "s/REPLACE_THIS/$H/" MAPR_HOME/data/properties.json
 service mapr-installer start
 sleep 10
 curl -k -I https://localhost:9443
 
 echo "Installer state: $?" > /tmp/mapr_installer_state
 
-input=$M_HOME/stanza_input.yml
+input=$MAPR_HOME/stanza_input.yml
 rm -f $input
 touch $input
-chown $M_USER:$M_USER $input
+chown $MAPR_USER:$MAPR_USER $input
 
 # TODO: get SSH user name
 echo "config.ssh_id=centos " >> $input
@@ -81,7 +81,7 @@ echo "config.cluster_name=$CLUSTER_NAME " >> $input
 echo "config.hosts=$NODE_LIST " >> $input
 echo "config.services={\"${SERVICE_TEMPLATE}\":{}} " >> $input
 
-CMD="cd $M_HOME; bin/mapr-installer-cli install -f -n -t $STANZA_URL -u $MAPR_USER:$MAPR_PASSWORD@localhost:9443 -o @$input"
+CMD="cd $MAPR_HOME; bin/mapr-installer-cli install -f -n -t $STANZA_URL -u $MAPR_USER:$MAPR_PASSWORD@localhost:9443 -o @$input"
 echo $CMD > /tmp/cmd
 
-sudo -u $M_USER bash -c "$CMD" || STATUS="FAILURE"
+sudo -u $MAPR_USER bash -c "$CMD" || STATUS="FAILURE"
