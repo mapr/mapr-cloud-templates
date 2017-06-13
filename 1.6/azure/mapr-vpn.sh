b33283402905ed4bed6a25342739f9a323ccef18
Scott Frolich <scottfrolich@sfrolic-E758.local>
2017-06-13 12:51:41 -0700

    undo logs

--- 1.6/azure/mapr-vpn.sh
+++ 1.6/azure/mapr-vpn.sh
@@ -5,32 +5,32 @@ admin_pw=$2
 subnet=$3
 listen_fqdn=$4
 
-echo "OPENVPN USER: $admin_user" >> ./mapr-vpn.log
-echo "OPENVPN PASSWORD: <hidden>" >> ./mapr-vpn.log
-echo "OPENVPN SUBNET: $subnet" >> ./mapr-vpn.log
-echo "OPENVPN LISTEN FQDN: $listen_fqdn" >> ./mapr-vpn.log
+echo "OPENVPN USER: $admin_user"
+echo "OPENVPN PASSWORD: <hidden>"
+echo "OPENVPN SUBNET: $subnet"
+echo "OPENVPN LISTEN FQDN: $listen_fqdn"
 
 # if OpenVPN is already installed don't do it again
 service --status-all 2>&1 | grep openvpnas > /dev/null
 if [ $? -eq 0 ]; then
-    echo "OpenVPN is already installed so it will not be reinstalled" >> ./mapr-vpn.log
+    echo "OpenVPN is already installed so it will not be reinstalled"
     exit 0
 fi
-echo "OpenVPN is not installed; installing ..." >> ./mapr-vpn.log
+echo "OpenVPN is not installed; installing ..."
 
 ./install_openvpn_access_server.sh $admin_pw
 last_exit=$?
 if [ $last_exit -ne 0 ]; then
-    >&2 echo "OpenVPN Error from install_openvpn_access_server: $last_exit" >> ./mapr-vpn.log
+    >&2 echo "OpenVPN Error from install_openvpn_access_server: $last_exit"
     exit $last_exit
 fi
-echo "OpenVPN successfully installed" >> ./mapr-vpn.log
+echo "OpenVPN successfully installed"
 
 ./mapr-vpn-configure.sh $admin_user $admin_pw $subnet $listen_fqdn
 last_exit=$?
 if [ $last_exit -ne 0 ]; then
-    >&2 echo "OpenVPN Error from mapr-vpn-configure: $last_exit" >> ./mapr-vpn.log
+    >&2 echo "OpenVPN Error from mapr-vpn-configure: $last_exit"
     exit $last_exit
 fi
-echo "OpenVPN successfully configured" >> ./mapr-vpn.log
-echo "OpenVPN setup complete" >> ./mapr-vpn.log
+echo "OpenVPN successfully configured"
+echo "OpenVPN setup complete"
