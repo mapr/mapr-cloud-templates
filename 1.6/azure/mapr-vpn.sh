cdb46b633ab5760a0520a3344e019f85afad7c1c
Scott Frolich <scottfrolich@sfrolic-E758.local>
2017-06-27 10:11:53 -0700

    pass in address space to OpenVPN settings

--- 1.6/azure/mapr-vpn.sh
+++ 1.6/azure/mapr-vpn.sh
@@ -4,11 +4,13 @@ admin_user=$1
 admin_pw=$2
 subnet=$3
 listen_fqdn=$4
+address_space=$5
 
 echo "OPENVPN USER: $admin_user"
 echo "OPENVPN PASSWORD: <hidden>"
 echo "OPENVPN SUBNET: $subnet"
 echo "OPENVPN LISTEN FQDN: $listen_fqdn"
+echo "OPENVPN ADDRESS SPACE: $address_space"
 
 # if OpenVPN is already installed don't do it again
 service --status-all 2>&1 | grep openvpnas > /dev/null
@@ -26,7 +28,7 @@ if [ $last_exit -ne 0 ]; then
 fi
 echo "OpenVPN successfully installed"
 
-./mapr-vpn-configure.sh $admin_user $admin_pw $subnet $listen_fqdn
+./mapr-vpn-configure.sh $admin_user $admin_pw $subnet $listen_fqdn $address_space
 last_exit=$?
 if [ $last_exit -ne 0 ]; then
     >&2 echo "OpenVPN Error from mapr-vpn-configure: $last_exit"
